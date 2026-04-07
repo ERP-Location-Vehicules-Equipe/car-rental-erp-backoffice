b5a7ef742b9fadf5e066f424194130943a58f349
Saidouchrif <166962901+Saidouchrif@users.noreply.github.com>
2026-04-07 13:33:37 +0200

    version de branch asfoury clean

--- code/front_fleet/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="app-footer">
-      <p>© {new Date().getFullYear()} ERP Fleet Backoffice</p>
-    </footer>
-  )
-}
-
-export default Footer
